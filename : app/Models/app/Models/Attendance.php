132756650a47a66a1e1f63aeb83eaaab120a62b4
waltermadrigal <70739730+waltermadrigal@users.noreply.github.com>
2026-09-22 12:43:15 -0300

    Create Attendance.php

---
+++ : app/Models/app/Models/Attendance.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Attendance extends Model
+{
+    protected $fillable = [
+        'user_id', 'device_id', 'check_in', 'check_out',
+        'signature_in', 'signature_out', 'status'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function device()
+    {
+        return $this->belongsTo(Device::class);
+    }
+}
